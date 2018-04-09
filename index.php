8a0d46249c32d380d00f5ab7fce4fc201f9e161c
Lucas Amparo <lucasamparo.ti@gmail.com>
2018-04-09 13:26:10 -0300

    Subindo o site

---
+++ index.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->  
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->  
+<!--[if !IE]><!--> <html lang="pt_br"> <!--<![endif]-->  
+<head>
+    <title>Lucas Amparo's Researcher Portal</title>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Responsive HTML5 Resume/CV Template for Developers">
+    <meta name="author" content="Xiaoying Riley at 3rd Wave Media">    
+    <link rel="shortcut icon" href="favicon.ico">  
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,500,400italic,300italic,300,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    <!-- Global CSS -->
+    <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">   
+    <!-- Plugins CSS -->
+    <link rel="stylesheet" href="assets/plugins/font-awesome/css/font-awesome.css">
+    
+    <!-- Theme CSS -->  
+    <link id="theme-style" rel="stylesheet" href="assets/css/styles.css">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head> 
+
+<body>
+    <div class="wrapper">
+        <div class="sidebar-wrapper">
+            <div class="profile-container">
+                <img class="profile" src="assets/images/profile.png" alt="" />
+                <h1 class="name">Lucas Amparo</h1>
+                <h3 class="tagline">Programador PHP, Java & C.</h3>
+				<h3 class="tagline">Consultor de T.I..</h3>
+				<h3 class="tagline">Professor (Matemática, Física, Computação).</h3>
+            </div><!--//profile-container-->
+            
+            <div class="contact-container container-block">
+                <ul class="list-unstyled contact-list">
+                    <li class="email"><i class="fa fa-envelope"></i><a href="mailto: lucasamparo.ti@gmail.com">lucasamparo.ti</a></li>
+                    <li class="phone"><i class="fa fa-phone"></i>Não declarado</li>
+                    <li class="github"><i class="fa fa-github"></i><a href="http://github.com/lucasamparo" target="_blank">@lucasamparo</a></li>
+					<li class="bitbucket"><i class="fa fa-bitbucket"></i><a href="http://bitbucket.com/lucasamparo" target="_blank">@lucasamparo</a></li>
+                    <li class="facebook"><i class="fa fa-facebook"></i><a href="http://fb.me/lucasamparo" target="_blank">@lucasamparo</a></li>
+                </ul>
+            </div><!--//contact-container-->
+            <div class="education-container container-block">
+                <h2 class="container-block-title">Formação</h2>
+				<div class="item">
+                    <h4 class="degree">Mestrado em Ciência da Computação</h4>
+                    <h5 class="meta">Universidade Federal da Bahia</h5>
+                    <div class="time">2016 - Atualmente</div>
+                </div><!--//item-->
+                <div class="item">
+                    <h4 class="degree">Bacharel em Sistemas de Informação</h4>
+                    <h5 class="meta">Instituto Federal de Educação, Ciência e Tecnologia da Bahia</h5>
+                    <div class="time">2011 - 2015</div>
+                </div><!--//item-->
+				<div class="item">
+                    <h4 class="degree">Técnico em Informática</h4>
+                    <h5 class="meta">Instituto Federal de Educação, Ciência e Tecnologia da Bahia</h5>
+                    <div class="time">2007 - 2010</div>
+                </div><!--//item-->
+            </div><!--//education-container-->
+            
+            <div class="languages-container container-block">
+                <h2 class="container-block-title">Línguas</h2>
+                <ul class="list-unstyled interests-list">
+                    <li>Português <span class="lang-desc">(Nativo)</span></li>
+                    <li>Inglês <span class="lang-desc">(Avançado)</span></li>
+                </ul>
+            </div><!--//interests-->
+            
+            <div class="interests-container container-block">
+                <h2 class="container-block-title">Linhas de Pesquisa</h2>
+                <ul class="list-unstyled interests-list">
+                    <li>Programação Web</li>
+					<li>Visão Computacional</li>
+					<li>Aprendizado de Máquina</li>
+					<li>Computação Gráfica</li>
+                </ul>
+            </div><!--//interests-->
+            
+        </div><!--//sidebar-wrapper-->
+        
+        <div class="main-wrapper">
+            
+            <section class="section summary-section">
+                <h2 class="section-title"><i class="fa fa-user"></i>Resumo</h2>
+                <div class="summary">
+                    <p>Lucas Amparo é um estudante/profissional de TI bahiano. Torcedor do Palmeiras e apaixonado por praticamente tudo que envolve tecnologia, está sempre em busca de um novo desafio.</p>
+                </div><!--//summary-->
+            </section><!--//section-->
+            
+            <section class="section experiences-section">
+                <h2 class="section-title"><i class="fa fa-briefcase"></i>Experiência</h2>
+                
+                <div class="item">
+                    <div class="meta">
+                        <div class="upper-row">
+                            <h3 class="job-title">Suporte Técnico</h3>
+                            <div class="time">2011 - 2012</div>
+                        </div><!--//upper-row-->
+                        <div class="company">Laboratório Oliveira, Vitória da Conquista - BA</div>
+                    </div><!--//meta-->
+                    <div class="details">
+                        <p>Manutenção Preventiva e Corretiva de Servidores. Operação dos Sistemas da Empresa. Gestão de Segurança de Informação.</p>
+                    </div><!--//details-->
+                </div><!--//item-->
+                
+                <div class="item">
+                    <div class="meta">
+                        <div class="upper-row">
+                            <h3 class="job-title">Programador PHP Pleno</h3>
+                            <div class="time">2014 - 2015</div>
+                        </div><!--//upper-row-->
+                        <div class="company">Átomo Tecnologia, Vitória da Conquista - BA</div>
+                    </div><!--//meta-->
+                    <div class="details">
+                        <p>Planejamento, desenvolvimento e manutenção de sistemas web utilizando PHP, JS, HTML, CSS e base de dados MySQL.</p>                          
+                    </div><!--//details-->
+                </div><!--//item-->
+                
+                <div class="item">
+                    <div class="meta">
+                        <div class="upper-row">
+                            <h3 class="job-title">Professor de Exatas</h3>
+                            <div class="time">2012 -2013; 2014 - 2015</div>
+                        </div><!--//upper-row-->
+                        <div class="company">Curso Edro, Vitória da Conquista - BA</div>
+                    </div><!--//meta-->
+                    <div class="details">
+                        <p>Planejamento, execução e acompanhamento didático nas disciplinas da área de exatas (física, química, matemática) para estudantes de nível fundamental e médio.</p>  
+                    </div><!--//details-->
+                </div><!--//item-->
+                
+            </section><!--//section-->
+            
+            <section class="section projects-section">
+                <h2 class="section-title"><i class="fa fa-archive"></i>Projetos</h2>
+                <div class="intro">
+                    <p>Executei e executo diversos projetos sistemas web e desktop. A maioria em PHP e alguns em C++.</p>
+                </div><!--//intro-->
+                <div class="item">
+                    <span class="project-title"><a href="#">iMestre</a></span> - <span class="project-tagline">Sistema Web de auxílio a professores.</span>
+                    
+                </div><!--//item-->
+            </section><!--//section-->
+			
+			<section class="section projects-section">
+                <h2 class="section-title"><i class="fa fa-book"></i>Publicações</h2>
+                <div class="intro">
+                    <p>Durante minha vida acadêmica, publiquei alguns artigos da mais variada relevância.</p>
+                </div><!--//intro-->
+				<h5>Artigos em Congressos/Workshops/Eventos em geral</h5>
+                <div class="item">
+                    <span class="project-title"><a href="https://www.academia.edu/5923506/CERTEB_Sistema_de_Emiss%C3%A3o_de_Certificado_Online">CERTEB - Sistema de Emissão de Certificados Online</a></span>
+                </div><!--//item-->
+				<div class="item">
+                    <span class="project-title"><a href="https://www.academia.edu/25430987/IFBuy_Um_Sistema_de_Processos_de_Compras_Em_Uma_Perspectiva_Interdisciplinar">IFBuy - Um Sistema de Processos de Compras em Uma Perspectiva Interdisciplinar</a></span>
+                </div><!--//item-->
+				<h5>Trabalhos de Títulos</h5>
+				<div class="item">
+                    <span class="project-title"><a href="https://www.academia.edu/25430854/iMestre_-_Sistema_Web_de_Aux%C3%ADlio_a_Professores">iMestre - Sistema Web de Auxílio a Professores</a> (Monografia)</span>
+                </div><!--//item-->
+				<h5>Como Co-Autor</h5>
+				<div class="item">
+                    <span class="project-title"><a href="https://www.academia.edu/7918302/e-Vent-Br_Proposta_de_um_Sistema_Web_de_Gerenciamento_de_Eventos_Acad%C3%AAmicos">e-Vent-BR: Proposta de um Sistema de Gerenciamento de Eventos Acadêmicos</a></span>
+                </div><!--//item-->
+				<h5>Rascunhos</h5>
+				<div class="item">
+                    <span class="project-title"><a href="https://www.academia.edu/29620888/Uso_de_impress%C3%A3o_digital_para_biometria">Uso de impressão digital para biometria</a></span>
+                </div><!--//item-->
+            </section><!--//section-->
+            
+            <section class="skills-section section">
+                <h2 class="section-title"><i class="fa fa-rocket"></i>Habilidades</h2>
+                <div class="skillset">
+                    
+                    <div class="item">
+                        <h3 class="level-title">Javascript &amp; jQuery</h3>
+                        <div class="level-bar">
+                            <div class="level-bar-inner" data-level="98%">
+                            </div>                                      
+                        </div><!--//level-bar-->                                 
+                    </div><!--//item-->
+					
+					<div class="item">
+                        <h3 class="level-title">C++</h3>
+                        <div class="level-bar">
+                            <div class="level-bar-inner" data-level="98%">
+                            </div>                                      
+                        </div><!--//level-bar-->                                 
+                    </div><!--//item-->
+                    
+                    <div class="item">
+                        <h3 class="level-title">PHP</h3>
+                        <div class="level-bar">
+                            <div class="level-bar-inner" data-level="98%">
+                            </div>                                      
+                        </div><!--//level-bar-->                                 
+                    </div><!--//item-->
+                    
+                    <div class="item">
+                        <h3 class="level-title">HTML5 &amp; CSS</h3>
+                        <div class="level-bar">
+                            <div class="level-bar-inner" data-level="80%">
+                            </div>                                      
+                        </div><!--//level-bar-->                                 
+                    </div><!--//item-->
+                    
+                    <div class="item">
+                        <h3 class="level-title">SQL</h3>
+                        <div class="level-bar">
+                            <div class="level-bar-inner" data-level="80%">
+                            </div>                                      
+                        </div><!--//level-bar-->                                 
+                    </div><!--//item-->
+					
+					<div class="item">
+                        <h3 class="level-title">Python</h3>
+                        <div class="level-bar">
+                            <div class="level-bar-inner" data-level="20%">
+                            </div>                                      
+                        </div><!--//level-bar-->                                 
+                    </div><!--//item-->
+                    
+                </div>  
+            </section><!--//skills-section-->
+            
+        </div><!--//main-body-->
+    </div>
+ 
+    <footer class="footer">
+        <div class="text-center">
+                <!--/* This template is released under the Creative Commons Attribution 3.0 License. Please keep the attribution link below when using for your own project. Thank you for your support. :) If you'd like to use the template without the attribution, you can check out other license options via our website: themes.3rdwavemedia.com */-->
+                <small class="copyright">Desenvolvido com <i class="fa fa-heart"></i> por <a href="http://themes.3rdwavemedia.com" target="_blank">Xiaoying Riley</a> para programadores</small>
+        </div><!--//container-->
+    </footer><!--//footer-->
+ 
+    <!-- Javascript -->          
+    <script type="text/javascript" src="assets/plugins/jquery-1.11.3.min.js"></script>
+    <script type="text/javascript" src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>    
+    <!-- custom js -->
+    <script type="text/javascript" src="assets/js/main.js"></script>            
+</body>
+</html> 
+
